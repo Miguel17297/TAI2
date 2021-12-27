981a9743bbd3e3b3836b4fd56195da20e2dfaf52
JoaoTS20 <joaots@ua.pt>
2021-12-27 23:03:10 +0000

    fix and new examples

--- Group1/src/locatelang.py
+++ Group1/src/locatelang.py
@@ -10,7 +10,7 @@ def read_folder(folder_path):
 
 
 def read_target(target_path):
-    with open(target_path, 'r') as file:
+    with open(target_path, 'r',encoding='utf-8') as file:
         return file.read()
 
 
@@ -22,25 +22,49 @@ class LocateLang:
         self.k = k
 
     def locate(self, target, k, threshold, window_size):
-
+        text_target=read_target(target)
+        results = {(i,i+k): {} for i in range(len(text_target)-k)}
         models = [Lang(filename, self.k, self.a, target) for filename in self.lang_refs]
-        results = {}
-
         for model in models:
-            bits = model.compute_bits_list(target)
+            bits = model.compute_bits_list(text_target)
             s_bits = self.suavization(bits, window_size)
-
-            initial_pos = None
             for i, b in enumerate(s_bits):
-
-                if b < threshold and not initial_pos:
-                    initial_pos = i
-
-                if initial_pos and b > threshold:
-                    results.setdefault(model.r, []).append((initial_pos, i + k))
-                    initial_pos = None
-
-        return results
+                results[(i,i+k)][model._r]=b
+        for pos in results:
+            if len([val for val  in results[pos].values() if val < threshold])==0:
+                for lan in list(results[pos]):
+                    if results[pos][lan] > min(results[pos].values()):
+                            results[pos].pop(lan)
+            else:
+                for lan in list(results[pos]):
+                    if results[pos][lan] > threshold:
+                            results[pos].pop(lan)
+    
+        short_results={}
+        positions={position[0] : position for position in list(results.keys())}
+        initial_pos=positions[0]
+        end_position=None
+        for pos in list(positions):
+            if pos!=list(positions)[-1]:
+                if results[initial_pos].keys()==results[positions[pos+1]].keys():
+                    end_position=positions[pos+1]
+                elif results[initial_pos].keys()!=results[positions[pos+1]].keys() and end_position is not None:
+                    short_results[(initial_pos[0],end_position[1])]=sorted(results[initial_pos], key=results[initial_pos].get)
+                    initial_pos=positions[pos+1]
+                    end_position=None
+                elif end_position is None:
+                    short_results[initial_pos]=sorted(results[initial_pos], key=results[initial_pos].get)
+                    initial_pos=positions[pos+1]
+            else:
+                if results[initial_pos].keys()==results[positions[pos]].keys():
+                    end_position=positions[pos]
+                    short_results[(initial_pos[0],end_position[1])]=sorted(results[initial_pos], key=results[initial_pos].get)
+                elif results[initial_pos].keys()!=results[positions[pos]].keys() and end_position is not None:
+                    short_results[(initial_pos[0],end_position[1])]=sorted(results[initial_pos], key=results[initial_pos].get)
+                    initial_pos=positions[pos]
+                    end_position=None
+        
+        return short_results, results
 
     def suavization(self, bits_list, window_size):
 
@@ -71,12 +95,21 @@ def main():
 
     if not threshold:
         threshold = math.log2(len(set(target))) / 2
-
+    print(f"Threshold: {threshold}")
     if not window_size:
         window_size = args.k
-
-    print(f"For target text:{args.target} we got {fl.locate(target, args.k, threshold, window_size)}")
-
+    print(f"Window Size: {window_size}")
+    
+    short_results, results =fl.locate(args.target, args.k, threshold, window_size)
+    
+    print(f"For target text:{args.target} we got:")
+    print("Long Results:")
+    for key in results:
+        print(f"{key} - {results[key]}")
+    print("-------------------------------------------")
+    print("Short Results:")
+    for key in short_results:
+        print(f"{key} - {short_results[key]}")
 
 if __name__ == "__main__":
     main()
